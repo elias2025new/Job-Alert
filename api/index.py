85eb5a1e552d6eb2ce96d73d599f9af9ff7ebe03
elias <eliashabibhamidabdu.gmail.com>
2026-04-23 17:42:03 +0300

    v

--- api/index.py
+++
@@ -1,8 +0,0 @@
-from http.server import BaseHTTPRequestHandler
-
-class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
-        self.end_headers()
-        self.wfile.write(b"Job Alert Bot is running. Cron runs daily at 9AM Addis Ababa time.")
